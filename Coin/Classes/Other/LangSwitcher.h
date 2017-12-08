e530bb6d4f34994a22d5fb78c4b15e91190a43ae
tinaleios <tianleios@163.com>
2017-12-08 10:28:30 +0800

    创建文件

---
+++ Coin/Classes/Other/LangSwitcher.h
@@ -0,0 +1,13 @@
+//
+//  LangSwitcher.h
+//  Coin
+//
+//  Created by  tianlei on 2017/12/08.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LangSwitcher : NSObject
+
+@end
---
+++ Coin/Classes/Other/LangSwitcher.m
@@ -0,0 +1,13 @@
+//
+//  LangSwitcher.m
+//  Coin
+//
+//  Created by  tianlei on 2017/12/08.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import "LangSwitcher.h"
+
+@implementation LangSwitcher
+
+@end
