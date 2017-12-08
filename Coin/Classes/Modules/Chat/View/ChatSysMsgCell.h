1cdcdf95333475483397c36c39a4d93f3aa369b9
tinaleios <tianleios@163.com>
2017-12-08 21:05:18 +0800

    test

---
+++ Coin/Classes/Modules/Chat/View/ChatSysMsgCell.h
@@ -0,0 +1,13 @@
+//
+//  ChatSysMsgCell.h
+//  Coin
+//
+//  Created by  tianlei on 2017/12/08.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatSysMsgCell : UITableViewCell
+
+@end
