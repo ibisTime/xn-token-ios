3d42b2e18681f8e3bcb8044c33f61ea0f427663a
czyGod <a549102100@163.com>
2017-12-09 20:14:19 +0800

    "个性设置创建文件"

---
+++ Coin/Classes/Modules/我的/Controller/PersonalitySetting/PersonalitySettingVC.m
@@ -0,0 +1,28 @@
+//
+//  PersonalitySettingVC.m
+//  Coin
+//
+//  Created by 蔡卓越 on 2017/12/9.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import "PersonalitySettingVC.h"
+
+@interface PersonalitySettingVC ()
+
+@end
+
+@implementation PersonalitySettingVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
