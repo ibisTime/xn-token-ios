dde8c044b29b713facde625dcd59bfcd86f319fb
tinaleios <tianleios@163.com>
2017-12-09 17:28:03 +0800

    wenjian

---
+++ Coin/Classes/Modules/交易/View/TradeDisplayView.h
@@ -0,0 +1,13 @@
+//
+//  TradeDisplayView.h
+//  Coin
+//
+//  Created by  tianlei on 2017/12/09.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TradeDisplayView : UIView
+
+@end
---
+++ Coin/Classes/Modules/交易/View/TradeDisplayView.m
@@ -0,0 +1,21 @@
+//
+//  TradeDisplayView.m
+//  Coin
+//
+//  Created by  tianlei on 2017/12/09.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import "TradeDisplayView.h"
+
+@implementation TradeDisplayView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
