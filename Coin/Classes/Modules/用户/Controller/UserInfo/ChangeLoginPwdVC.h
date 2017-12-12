de6d31478c5c9e42f32a256af76557b73210a0f8
czyGod <a549102100@163.com>
2017-12-12 15:20:29 +0800

    "添加修改登录密码文件"

---
+++ Coin/Classes/Modules/用户/Controller/UserInfo/ChangeLoginPwdVC.h
@@ -0,0 +1,13 @@
+//
+//  ChangeLoginPwdVC.h
+//  Coin
+//
+//  Created by 蔡卓越 on 2017/12/12.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import "TLAccountSetBaseVC.h"
+
+@interface ChangeLoginPwdVC : TLAccountSetBaseVC
+
+@end
---
+++ Coin/Classes/Modules/用户/Controller/UserInfo/ChangeLoginPwdVC.m
@@ -0,0 +1,29 @@
+//
+//  ChangeLoginPwdVC.m
+//  Coin
+//
+//  Created by 蔡卓越 on 2017/12/12.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import "ChangeLoginPwdVC.h"
+
+@interface ChangeLoginPwdVC ()
+
+@end
+
+@implementation ChangeLoginPwdVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
